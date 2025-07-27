bfe6b91b12e55e0d87367047c2d57eb1d3477fdf
omid <araghiom@gmail.com>
2025-07-28 00:53:27 +0330

    Add SIDEBAR_MENU constant with resource and related site entries for navigation structure.

---
+++ src/constants/sidebarMenu.js
@@ -0,0 +1,20 @@
+export const SIDEBAR_MENU = {
+  RESOURCES_OVERVIEW: [
+
+    {
+      id: 'tools',
+      label: 'Tools and Guides',
+      icon: 'build',
+      route: '/resources/tools'
+    },
+    {
+      id: 'news',
+      label: 'News and Information',
+      icon: 'article',
+      route: '/resources/news'
+    }
+  ],
+  RELATED_SITES: [
+   
+  ]
+} 
