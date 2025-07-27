c7b11ade6d251bf382d1346bd04e6026aa1f6a62
omid <araghiom@gmail.com>
2025-07-28 01:50:25 +0330

    Refactor MainLayout to enhance structure and styling. Introduce new main grid layout, update header and footer classes, and integrate CommentFooter component. Improve sidebar and page content organization with CSS adjustments for better responsiveness.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,21 +1,25 @@
 <template>
   <q-layout view="hHh lpR fFf" class="flex flex-column fixed-layout">
-    <header class="flex-none">
+    <header class="header-container">
       <layout-primary-header />
       <layout-secondary-header />
-      <layout-sidebar-menu />
     </header>
     
-    <q-page-container class="flex-grow overflow-auto w-full">
-      <router-view v-slot="{ Component }">
-        <transition name="fade" mode="out-in">
-          <component :is="Component" />
-        </transition>
-      </router-view>
-    </q-page-container>
+    <main class="main-grid">
+      <layout-sidebar-menu class="sidebar" />
+      <q-page-container class="page-content flex-grow overflow-auto w-full">
+        <router-view v-slot="{ Component }" class="page-content-container">
+          <transition name="fade" mode="out-in">
+            <component :is="Component" />
+          </transition>
+        </router-view>
+      </q-page-container>
+    </main>
 
-    <footer class="flex-none">
-  <layout-app-footer />
+    <footer class="footer-container">
+      <layout-comment-footer />
+      <layout-app-footer />
+
     </footer>
   </q-layout>
 </template>
@@ -25,7 +29,7 @@ import LayoutPrimaryHeader from '@/components/layout/PrimaryHeader.vue'
 import LayoutSecondaryHeader from '@/components/layout/SecondaryHeader.vue'
 import LayoutSidebarMenu from '@/components/layout/SidebarMenu.vue'
 import LayoutAppFooter from '@/components/layout/AppFooter.vue'
-
+import LayoutCommentFooter from '@/components/layout/CommentFooter.vue'
 export default {
   name: 'MainLayout',
   components: {
@@ -33,6 +37,7 @@ export default {
     LayoutSecondaryHeader,
     LayoutSidebarMenu,
     LayoutAppFooter,
+    LayoutCommentFooter
   }
 }
 </script>
@@ -45,13 +50,38 @@ export default {
   width: 100%;
 }
 
+.header-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 150px;
+}
+.main-grid {
+  width:  100%;
+  display: grid;
+  grid-template-columns: 250px 1fr
+  ;
+  height: calc(100vh - 150px - 200px); 
+  padding: 0 10px;
+}
+
+
+.sidebar {
+  width: 300px;
+  overflow-y: auto;
+  background-color: white;
+}
+
+.page-content {
+  overflow-y: auto;
+}
+
 .w-full {
   width: 100%;
 }
 
 .overflow-auto {
   overflow-y: auto;
-  height: calc(100vh - (header height + footer height));
 }
 
 .fade-enter-active,
@@ -62,5 +92,11 @@ export default {
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
+}
+.footer-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 200px;
 }
 </style> 
